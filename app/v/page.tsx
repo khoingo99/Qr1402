e17a9d345d30c4a2ecb9d637cfc50758c4bf7475
DESKTOP-108HBU1\Vision <dev>
2026-02-05 14:33:55 +0900

    edit

--- app/v/page.tsx
+++ app/v/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({
   const sp = await searchParams;
 
   const to = (sp.to ?? "Thanh Chúc").slice(0, 40);
-  const msg = (sp.msg ?? "Em mãi là công chúa của anh").slice(0, 120);
+  const msg = (sp.msg ?? "Cảm ơn em đã cho anh cơ hội").slice(0, 120);
   const date = (sp.date ?? "14/02/2026").slice(0, 10);
 
   return (
