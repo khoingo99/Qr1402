fdfaed37717628422a6e04455269584d4f9a07ac
DESKTOP-108HBU1\Vision <dev>
2026-02-05 12:42:32 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import LinkMaker from "@/components/LinkMaker";
 
 export default function Home() {
--- app/v/page.tsx
+++ app/v/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import LoveScene from "@/components/LoveScene";
 
 export default async function Page({
